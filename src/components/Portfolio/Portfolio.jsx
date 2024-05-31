5cc57aefae20a4cc5cdeca5bf8f257fea70bddd8
Wallace143900 <hiperguardi@gmail.com>
2024-05-30 23:30:35 -0300

    em desenvolvimento

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,10 @@
+import {MyProjects} from "../Portfolio/myProjects/myProjects";
+
 export const Portfolio = () => {
     return (
         <section>
             <h2>Portfólio</h2>
-            
+            <MyProjects />
         </section>
     )
 }
---
+++ src/components/Portfolio/myProjects/myProjects.jsx
@@ -0,0 +1,84 @@
+export const MyProjects = () => {
+    return (
+        <article>
+                <div>
+                    <h3>Habilidades usadas</h3>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <div>
+                        <a href="" target="blanck"></a>
+                    </div>
+                    <div>
+                        <h3>Nome do Projeto</h3>
+                        <p>expecificação do projeto</p>
+                    </div>
+                </div>
+            <div>
+                <h3>Habilidades usadas</h3>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <div>
+                    <a href="" target="blanck"></a>
+                </div>
+                <div>
+                    <h3>Nome do Projeto</h3>
+                    <p>expecificação do projeto</p>
+                </div>
+            </div>
+        <div>
+            <h3>Habilidades usadas</h3>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <div>
+                <a href="" target="blanck"></a>
+            </div>
+            <div>
+                <h3>Nome do Projeto</h3>
+                <p>expecificação do projeto</p>
+            </div>
+        </div>
+    <div>
+        <h3>Habilidades usadas</h3>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <div>
+            <a href="" target="blanck"></a>
+        </div>
+        <div>
+            <h3>Nome do Projeto</h3>
+            <p>expecificação do projeto</p>
+        </div>
+    </div>
+                <div>
+                    <h3>Habilidades usadas</h3>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <div>
+                        <a href="" target="blanck"></a>
+                    </div>
+                    <div>
+                        <h3>Nome do Projeto</h3>
+                        <p>expecificação do projeto</p>
+                    </div>
+                </div>
+            <div>
+                <h3>Habilidades usadas</h3>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <div>
+                    <a href="" target="blanck"></a>
+                </div>
+                <div>
+                    <h3>Nome do Projeto</h3>
+                    <p>expecificação do projeto</p>
+                </div>
+            </div>
+        </article>
+    )
+}
