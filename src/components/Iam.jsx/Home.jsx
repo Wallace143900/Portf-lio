81747bc4b52728fea6286352c954229481957e15
Wallace143900 <hiperguardi@gmail.com>
2024-07-02 13:12:43 -0300

    atualizei meu currículo

--- src/components/Iam.jsx/Home.jsx
+++ src/components/Iam.jsx/Home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
                     <h1 className="title1" id={styles.title1}>Wallace Menezes</h1>
                     <h4 className="title4">Desenvolvedor Front End</h4>
                     <div className={styles.divButtons}>
-                        <a href="https://drive.google.com/file/d/11eA9spxr-FfXXg3AJJwzpfWHZhIinUHv/view?usp=drive_link" className="buttonCV" id={styles.buttonCV} target="_blank">Download CV</a>
+                        <a href="https://drive.google.com/file/d/1Dj8Gc_1CCEwDNTEIrDwlPFHpcZw4Abks/view?usp=sharing" className="buttonCV" id={styles.buttonCV} target="_blank">Download CV</a>
                         <a href="https://wa.me/5521965629403" className="buttonContato" id={styles.buttonContato} target="_blank">Me Chame</a>
                     </div>
                 </div>
